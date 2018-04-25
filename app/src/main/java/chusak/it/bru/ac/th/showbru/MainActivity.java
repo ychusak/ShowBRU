038de8ec4f67b7fdc61be042ec37dcbd67742b34
Chusak <chusak.bru@gmail.com>
2018-04-25 14:08:42 +0700

    Initial commit

---
+++ app/src/main/java/chusak/it/bru/ac/th/showbru/MainActivity.java
@@ -0,0 +1,19 @@
+package chusak.it.bru.ac.th.showbru;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import chusak.it.bru.ac.th.showbru.fragment.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        Add Fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.contentMainFragment, new MainFragment()).commit();
+        }
+    } // Main Method
+} // Main Class
---
+++ app/src/main/java/chusak/it/bru/ac/th/showbru/fragment/MainFragment.java
@@ -0,0 +1,20 @@
+package chusak.it.bru.ac.th.showbru.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import chusak.it.bru.ac.th.showbru.R;
+
+public class MainFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+} // Main Class
